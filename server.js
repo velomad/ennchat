ba0513f87d579950d9f1d984e63074e2ad139021
Velomad <sagaryc111@gmail.com>
2021-04-16 03:04:42 +0530

    asda

---
+++ server.js
@@ -0,0 +1,30 @@
+const io = require("socket.io")(5000);
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
+
+io.on("connection", (socket) => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
+
+  socket.on("send-message", ({ recipients, text }) => {
+    recipients.forEach((recipient) => {
+      const newRecipients = recipients.filter((r) => r !== recipient);
+      newRecipients.push(id);
+      socket.broadcast.to(recipient).emit("receive-message", {
+        recipients: newRecipients,
+        sender: id,
+        text,
+      });
+    });
+  });
+});
