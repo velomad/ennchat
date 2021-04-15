8c5bec2bf5ad072b95aea5ea1589d7c855a27645
Velomad <sagaryc111@gmail.com>
2021-04-16 03:05:50 +0530

    build push

---
+++ build/precache-manifest.58027e4897d7f3d7963d4887031cce27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc18b31975c774c08ba7055cf74201b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed78c6102d1a03003781",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ed78c6102d1a03003781",
+    "url": "/static/js/2.ba15c0f8.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.ba15c0f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2622d6a56bb65408819f",
+    "url": "/static/js/main.c0bba669.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
